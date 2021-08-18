36e3441d59b456c9fc9d89779eed88b386227d06
Luis Moreno <l.david1929@gmail.com>
2021-08-18 16:00:18 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
-from pathlib import Path
 
 setuptools.setup(
     name="igramscraper",
     version="0.3.5",
     description=('scrapes medias, likes, followers, tags and all metadata'),
-    long_description=Path("README.md").read_text(),
+    long_description=("TODO"),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="MIT",
